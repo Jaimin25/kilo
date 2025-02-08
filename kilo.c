2bb841bcade3242f6ca856130987f58149866f4c
Jaimin Chovatia <chovatiajaimin@gmail.com>
2025-02-08 18:37:39 +0530

    Search

--- kilo.c
+++ kilo.c
@@ -304,6 +304,26 @@ int editorRowCxToRx(erow *row, int cx)
     return rx;
 }
 
+int editorRowRxToCx(erow *row, int rx)
+{
+    int cur_rx = 0;
+    int cx;
+
+    for (cx = 0; cx < row->size; cx++)
+    {
+        if (row->chars[cx] == '\t')
+        {
+            cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+        }
+        cur_rx++;
+
+        if (cur_rx > rx)
+            return cx;
+    }
+
+    return cx;
+}
+
 void editorInsertRow(int at, char *s, size_t len)
 {
     if (at < 0 || at > E.numrows)
@@ -512,6 +532,29 @@ void editorSave()
 
     free(buf);
     editorSetStatusMessage("Can\'t save! I/O error: %s", strerror(errno));
+}
+
+/*** FIND ***/
+
+void editorFind()
+{
+    char *query = editorPrompt("Search: %s (ESC to cancel)");
+    if (query == NULL)
+        return;
+    int i;
+    for (i = 0; i < E.numrows; i++)
+    {
+        erow *row = &E.row[i];
+        char *match = strstr(row->render, query);
+        if (match)
+        {
+            E.cy = i;
+            E.cx = editorRowRxToCx(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+    free(query);
 }
 
 /*  APPEND BUFER    */
@@ -816,6 +859,10 @@ void editorProcessKeypress()
         }
         break;
 
+    case CTRL_KEY('f'):
+        editorFind();
+        break;
+
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
@@ -896,7 +943,7 @@ int main(int argc, char *argv[])
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
     while (1)
     {
